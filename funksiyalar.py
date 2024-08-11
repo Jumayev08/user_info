c9ea328e45d93fd0753c4811bade48bf99df97f1
Jumayev Javohir <138192111+Jumayev08@users.noreply.github.com>
2024-08-11 18:06:57 +0500

    Update funksiyalar.py

--- funksiyalar.py
+++ funksiyalar.py
@@ -6,11 +6,11 @@ async def get_user_info(message: types.Message, bot: Bot):
     user_photos = await message.from_user.get_profile_photos()
     
     text=""
-    text+="Foydalanuvchi: "+str(message.from_user.full_name)+"\n"
+    text+="Foydalanuvchi: "+str(message.from_user.mention_html(message.from_user.full_name))+"\n"
     text+="ID: "+str(message.from_user.id)+"\n"
-    if user.username: text+="Foydalanuvchining username: @"+str(message.from_user.username)+"\n"
+    if user.username: text+="Username: @"+str(message.from_user.username)+"\n"
     if user.bio: text+="Bio: "+str(user.bio)+"\n"
     if user_photos.photos:
-        await message.answer_photo(user_photos.photos[0][-1].file_id,caption=text)
+        await message.answer_photo(user_photos.photos[0][-1].file_id,caption=text, parse_mode="HTML")
     else:
-        await message.answer(text)
+        await message.answer(text, parse_mode="HTML")
